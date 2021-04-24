a87b3833c4f2172d9039d770dee375af3e4a465d
xjyplayer <2532917800@qq.com>
2021-04-24 18:38:17 +0800

    init

--- install-nfs-client.sh
+++ install-nfs-client.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 kubectl create namespace nfs-client
-helm install nfs-client --set nfs.server=139.224.132.234 --set nfs.path=/some/container/path/dynamic --set storageClass.reclaimPolicy=Retain stable/nfs-client-provisioner --namespace nfs-client
+helm install nfs-client --set nfs.server=localhost.kvm-1 --set nfs.path=/some/container/path/dynamic --set storageClass.reclaimPolicy=Retain stable/nfs-client-provisioner --namespace nfs-client
